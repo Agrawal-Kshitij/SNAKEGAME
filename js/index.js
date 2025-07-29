87836c2e5d0513f621ed3f68885be8da07433e50
Agrawal-Kshitij <kshitijagrawal234@gmail.com>
2025-07-29 10:47:34 +0530

    Removal of old image and addition of new sounds and bgc

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 //CONSTANTS AND VARIABLES
 let inputDir = {x: 0 , y: 0};
-const foodSound = new Audio('snake-hiss-95241.mp3');
-const gameOverSound = new Audio('');
-const moveSound = new Audio('snake-hiss-95241.mp3');
-const musicSound = new Audio(''); 
+const foodSound = new Audio('food.mp3');
+const gameOverSound = new Audio('gameover.mp3');
+const moveSound = new Audio('move.mp3');
+//const musicSound = new Audio('music.mp3'); 
 let speed = 6;
 let score = 0;
 let lastPaintTime = 0;
@@ -15,7 +15,6 @@ food = {x: 6 , y: 7};
 //GAME FUNCTIONS
 function main(ctime){
     window.requestAnimationFrame(main);
-    //console.log(ctime);
     if((ctime - lastPaintTime)/1000 < 1/speed){
         return;
     }
